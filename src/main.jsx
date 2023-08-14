1d578b605d1667206a28b70431a07678b0d1bd34
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-14 12:46:59 +0200

    Install react-router-dom dependancies & create route to navigate to HomePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 
-import App from "./App";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" replace />,
+      },
+      {
+        path: "/home",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
