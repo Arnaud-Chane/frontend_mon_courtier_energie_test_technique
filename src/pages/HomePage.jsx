f2fd512d354d14905d9a478ba637137cfc0bac93
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-14 19:19:59 +0200

    Install dotenv and axios & Listing all tasks saved in database

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    const fetchTask = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/tasks`
+        );
+        setTaskList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchTask();
+  }, []);
+
+  return (
+    <div className="HomePage">
+      <ul className="task-list">
+        {taskList.map((task) => {
+          return (
+            <li className="task" key={task.task_id}>
+              <div className="task-to-edit">{task.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
