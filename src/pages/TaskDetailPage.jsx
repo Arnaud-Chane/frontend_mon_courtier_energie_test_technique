aadc5b60c69d99e4e25676410da585836a686696
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-15 11:19:05 +0200

    Getting id from params & useEffect to fetch task detail info & display task detail

--- src/pages/TaskDetailPage.jsx
+++ src/pages/TaskDetailPage.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function TaskDetailPage() {
-  return <div>TaskDetailPage</div>;
+  const { id } = useParams();
+  const [taskDetail, setTaskDetail] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/api/tasks/${id}`)
+      .then((response) => {
+        setTaskDetail(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
+  return <div>{taskDetail.title}</div>;
 }
 
 export default TaskDetailPage;
