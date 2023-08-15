94306e0d25fc9840b221dd4127e6906f23611249
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-15 16:02:44 +0200

    Adding form component antd to signup page for pseudo

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { Form, Input } from "antd";
 
 function SignUpPage() {
-  return <div>SignUpPage</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div className="SignUpPage">
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Pseudo"
+          name="pseudo"
+          rules={[
+            {
+              required: true,
+              message: "Veuillez renseigner votre identifiant.",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default SignUpPage;
