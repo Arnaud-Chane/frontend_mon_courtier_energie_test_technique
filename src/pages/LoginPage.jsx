565ffed146a86ccfad4a1b85e8162a60608f12af
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-15 15:56:47 +0200

    Adding form component of antd

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,70 @@
-import { Input } from "antd";
+import { Button, Form, Input } from "antd";
 
 function LoginPage() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
     <div className="LoginPage">
-      <Input placeholder="Email" />
-      <Input placeholder="Password" />
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Veuillez renseigner une adresse mail.",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Veuillez renseigner votre mot de passe.",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Connexion
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
