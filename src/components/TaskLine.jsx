79508042ca334f4c2ee747314b840b8aaf476fd2
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-17 18:24:26 +0200

    Creating component task Line to lighten the Home Page

--- src/components/TaskLine.jsx
+++ src/components/TaskLine.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import PropTypes from "prop-types";
+import axios from "axios";
+import { Button, Checkbox } from "antd";
+import { Link } from "react-router-dom";
 
-function TaskLine() {
+import EditIcon from "../assets/images/edit-icon.svg";
+import InputPriority from "./InputPriority";
+
+function TaskLine({ task, fetchData, setFetchData, taskList, setTaskList }) {
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.delete(
+        `${import.meta.env.VITE_BACKEND_URL}/api/tasks/${id}`
+      );
+      if (response.status === 204) {
+        setFetchData(!fetchData);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChecked = async (tasked) => {
+    const mapped = taskList.map((taskSolo) => {
+      return taskSolo.task_id === Number(tasked.task_id)
+        ? { ...taskSolo, task_done: !taskSolo.task_done }
+        : { ...taskSolo };
+    });
+    setTaskList(mapped);
+    const body = tasked;
+    body.task_done = !tasked.task_done;
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_BACKEND_URL}/api/tasks/is-done/${
+          tasked.task_id
+        }`,
+        body
+      );
+      if (response.status === 204) {
+        setFetchData(!fetchData);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div>TaskLine</div>
-  )
+    <div className="TaskLine">
+      <li className="task" key={task.task_id}>
+        <InputPriority
+          task={task}
+          setFetchData={setFetchData}
+          fetchData={fetchData}
+        />
+        <Checkbox
+          checked={task.task_done}
+          onChange={() => handleChecked(task)}
+        />
+        <div
+          className={task.task_done ? "task-to-edit strike" : "task-to-edit"}
+        >
+          {task.title}
+        </div>
+        <div className="due-date">{task.due_date}</div>
+        <div className="delete-btn-task">
+          <Link to={`/task/${task.task_id}`}>
+            <img className="edit-icon-homepage" src={EditIcon} alt="" />
+          </Link>
+          <Button type="submit" onClick={() => handleDelete(task.task_id)}>
+            delete
+          </Button>
+        </div>
+      </li>
+    </div>
+  );
 }
 
-export default TaskLine
+TaskLine.propTypes = {
+  task: PropTypes.node.isRequired,
+  fetchData: PropTypes.bool.isRequired,
+  setFetchData: PropTypes.func.isRequired,
+  taskList: PropTypes.array.isRequired,
+  setTaskList: PropTypes.func.isRequired,
+};
+
+export default TaskLine;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 // import dayjs from "dayjs";
-import { Button, Input, Checkbox, DatePicker, Space } from "antd";
+import { Button, Input, DatePicker, Space } from "antd";
 
-import EditIcon from "../assets/images/edit-icon.svg";
-import InputPriority from "../components/InputPriority";
+import TaskLine from "../components/TaskLine";
 
 function HomePage() {
   const [taskList, setTaskList] = useState([]);
@@ -73,43 +71,6 @@ function HomePage() {
     }
   };
 
-  const handleDelete = async (id) => {
-    try {
-      const response = await axios.delete(
-        `${import.meta.env.VITE_BACKEND_URL}/api/tasks/${id}`
-      );
-      if (response.status === 204) {
-        setFetchData(!fetchData);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const handleChecked = async (tasked) => {
-    const mapped = taskList.map((task) => {
-      return task.task_id === Number(tasked.task_id)
-        ? { ...task, task_done: !task.task_done }
-        : { ...task };
-    });
-    setTaskList(mapped);
-    const body = tasked;
-    body.task_done = !tasked.task_done;
-    try {
-      const response = await axios.put(
-        `${import.meta.env.VITE_BACKEND_URL}/api/tasks/is-done/${
-          tasked.task_id
-        }`,
-        body
-      );
-      if (response.status === 204) {
-        setFetchData(!fetchData);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   const handleTimePicker = (value, dateString) => {
     setDueDate(dateString);
   };
@@ -141,36 +102,14 @@ function HomePage() {
       <ul className="task-list">
         {taskList.sort(compareByPriority).map((task) => {
           return (
-            <li className="task" key={task.task_id}>
-              <InputPriority
-                task={task}
-                setFetchData={setFetchData}
-                fetchData={fetchData}
-              />
-              <Checkbox
-                checked={task.task_done}
-                onChange={() => handleChecked(task)}
-              />
-              <div
-                className={
-                  task.task_done ? "task-to-edit strike" : "task-to-edit"
-                }
-              >
-                {task.title}
-              </div>
-              <div className="due-date">{task.due_date}</div>
-              <div className="delete-btn-task">
-                <Link to={`/task/${task.task_id}`}>
-                  <img className="edit-icon-homepage" src={EditIcon} alt="" />
-                </Link>
-                <Button
-                  type="submit"
-                  onClick={() => handleDelete(task.task_id)}
-                >
-                  delete
-                </Button>
-              </div>
-            </li>
+            <TaskLine
+              task={task}
+              taskInfo={task}
+              setFetchData={setFetchData}
+              fetchData={fetchData}
+              taskList={taskList}
+              setTaskList={setTaskList}
+            />
           );
         })}
       </ul>
