cdc42303d5f79011db9fc6c834791ad96484341a
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-17 11:50:01 +0200

    Import and add Error 500 image & display text

--- src/pages/Page500.jsx
+++ src/pages/Page500.jsx
@@ -1,11 +1,18 @@
 import { Button } from "antd";
 import { useNavigate } from "react-router-dom";
 
+import Image500 from "../assets/images/500.png";
+
 function Page500() {
   const navigate = useNavigate();
 
   return (
     <div className="Page500">
+      <img src={Image500} alt="500 error icon" />
+      <div className="content">
+        Merci de contacter le support, il y a dû y avoir un problème de notre
+        part.
+      </div>
       <Button type="primary" onClick={() => navigate("/")}>
         Revenir à la page d'accueil
       </Button>
