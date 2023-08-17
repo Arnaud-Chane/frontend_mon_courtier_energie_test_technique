17496af5993b5bcd81bf2219f64796ef48bc2bde
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-17 09:25:55 +0200

    Navbar visible when user if connected

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect, useContext } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
+import { UserInfoContext } from "../context/UserRoleContext";
+
 export default function Root() {
+  const { userInfo } = useContext(UserInfoContext);
+
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  useEffect(() => {
+    if ("token" in window.localStorage) {
+      setShowNavbar(true);
+    }
+  }, [userInfo.role]);
+
   return (
     <>
-      <Navbar />
+      {showNavbar && <Navbar />}
       <main>
         <Outlet />
       </main>
