132aa32250a90b81f70100bf451b34b474a9ca33
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-17 09:57:04 +0200

    Display list of all user : useEffect to fetch all user, set info in userList, map userList

--- src/pages/admin/UserListPage.jsx
+++ src/pages/admin/UserListPage.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function UserListPage() {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/users`
+        );
+        setUserList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div className="UserListPage">
-      UserListPage
+      {userList.map((user) => {
+        return (
+          <li key={user.user_id}>{user.pseudo} - {user.email}</li>
+        );
+      })}
     </div>
   );
 }
