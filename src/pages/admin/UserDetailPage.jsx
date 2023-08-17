098f31a8b7419f1ae31660f9de2c9fd3a55cd419
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-17 10:02:44 +0200

    Import user context & display user pseudo

--- src/pages/admin/UserDetailPage.jsx
+++ src/pages/admin/UserDetailPage.jsx
@@ -1,11 +1,16 @@
-import { Button } from "antd";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "antd";
+import { UserInfoContext } from "../../context/UserRoleContext";
 
 function UserDetailPage() {
   const navigate = useNavigate();
 
+  const { userInfo } = useContext(UserInfoContext);
+
   return (
     <div className="UserDetailPage">
+      <div className="user-pseudo">{userInfo.pseudo}</div>
       <Button type="primary" onClick={() => navigate("/admin")}>
         Annuler
       </Button>
