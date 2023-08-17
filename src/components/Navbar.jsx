406d8131b99b9c7e03117d2257ac38addc27fce5
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-17 12:18:36 +0200

    Condition on user role with usecontext > Display admin link navbar if admin and user link if user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import LogoMCE from "../assets/images/logo-mce.png";
+import { UserInfoContext } from "../context/UserRoleContext";
 
 function Navbar() {
   const navigate = useNavigate();
+  const { userInfo } = useContext(UserInfoContext);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -14,6 +17,11 @@ function Navbar() {
       <Link to="/">
         <img src={LogoMCE} alt="Logo MCE" />
       </Link>
+      {userInfo.role === 1 ? (
+        <Link to="/admin">Liste des membres</Link>
+      ) : (
+        <Link to="/profile">Profil</Link>
+      )}
       <button type="submit" className="logout" onClick={handleLogout}>
         Déconnexion
       </button>
