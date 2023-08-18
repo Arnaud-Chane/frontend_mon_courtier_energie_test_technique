b3f6baf886438e129c0abaf2635b378062f35869
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-18 10:50:59 +0200

    Ajusting title header task & inverse color btn of detail task page

--- src/main.jsx
+++ src/main.jsx
@@ -28,7 +28,6 @@ import UserDetailPage from "./pages/admin/UserDetailPage";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
     children: [
       {
         index: true,
